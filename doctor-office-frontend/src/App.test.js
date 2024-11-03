19c0fcd28edf138c93d574b6c79ad43033fba27d
deeksha-2825 <deeksharamakrishna28@gmail.com>
2024-11-03 15:49:08 -0500

    unit test file for frontend

--- doctor-office-frontend/src/App.test.js
+++ doctor-office-frontend/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Doctor\'s Office Appointments header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/Doctor's Office Appointments/i);
+  expect(headerElement).toBeInTheDocument();
 });
+
